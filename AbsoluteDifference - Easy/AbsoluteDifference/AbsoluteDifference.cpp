787a7af358e6e33ba32f07c351ef9538311f951a
Johanna Johansson <johannajohansson642@gmail.com>
2022-02-21 21:32:49 +0100

    assigment lastt didit of number & the extra two on increase difficulty. added cin.good on the first assigment to check if its an integer

--- AbsoluteDifference - Easy/AbsoluteDifference/AbsoluteDifference.cpp
+++ AbsoluteDifference - Easy/AbsoluteDifference/AbsoluteDifference.cpp
@@ -11,12 +11,30 @@ int main()
 {
     int count = 1;
     int numbers[2];
+    bool isInt;
   
     cout << "-- Give two numbers -- " << endl;
     for (int i = 0; i < 2; i++)
     {
         cout << "Number" << count << " : ";
         cin >> numbers[i];
+        while (1)
+        {
+            if (!cin.good())
+            {
+                cin.clear();
+                cin.ignore(INT_MAX, '\n');
+                cout << "Not an integer try again!" << endl;
+                cout << "Number" << count << " : ";
+                cin >> numbers[i];
+            }
+            else
+            {
+                cin.clear();
+                cin.ignore(INT_MAX, '\n');
+                break;
+            }
+        }
         count++;
     }
    
---
+++ AbsoluteDifference - Easy/LastDigitOfANumber/LastDigitOfANumber.cpp
@@ -0,0 +1,53 @@
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int ReturnLastDigit(int input);
+void PrintNumberReversed(int input);
+void PrintAllNumbers(int input);
+
+int main()
+{
+	int input;
+	string inputString;
+
+	cout << "-- Enter a number --" << endl;
+	cin >> input;
+	
+	cout << "The last digit in that number is: " << ReturnLastDigit (abs(input)) << endl;
+	cout << "All the digets reversed is: "; PrintNumberReversed(abs(input));
+	cout << endl;
+	cout << "All the digets is: "; PrintAllNumbers(abs(input));
+	cout << endl;
+}
+int ReturnLastDigit(int input)
+{
+	int lastDigit;
+	lastDigit = input % 10;
+	return lastDigit;
+}
+void PrintAllNumbers(int input)
+{
+	int digit;
+
+	if (input >= 10)
+	{
+		PrintAllNumbers(input / 10);
+	}
+	digit = input % 10;
+	cout << digit << " ";
+}
+void PrintNumberReversed(int input)
+{
+	int digit = 0;
+	while (input > 0)
+	{ 
+		digit *= 10;
+		digit = input % 10;
+		input /= 10;
+		cout << digit << " ";
+	}
+}
+
